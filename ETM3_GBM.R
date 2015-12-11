df753edace15e6a8feec9dfa8de5ba1bfb432564
mgwalsh <markusgwalsh@gmail.com>
2015-12-11 16:14:41 +0300

    Generalized boosted regression

--- ETM3_GBM.R
+++ ETM3_GBM.R
@@ -3,7 +3,7 @@
 #' M. Walsh, December 2015
 
 # Required packages
-# install.packages(c("devtools","caret","plyr",gbm")), dependencies=TRUE)
+# install.packages(c("devtools","caret","plyr","gbm")), dependencies=TRUE)
 require(devtools)
 require(caret)
 require(plyr)
