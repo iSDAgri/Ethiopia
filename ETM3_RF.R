8d3702ffef0cf158309ee309d5ff647e1fab4d56
mgwalsh <markusgwalsh@gmail.com>
2015-12-12 18:27:07 +0300

    randomForest predictions

--- ETM3_RF.R
+++ ETM3_RF.R
@@ -3,9 +3,10 @@
 #' M. Walsh, December 2015
 
 # Required packages
-# install.packages(c("devtools","caret","randomForest","raster")), dependencies=TRUE)
+# install.packages(c("devtools","caret","doParallel","randomForest","raster")), dependencies=TRUE)
 require(devtools)
 require(caret)
+require(doParallel)
 require(randomForest)
 require(raster)
 
@@ -25,40 +26,45 @@ GRIDSc <- etm3_cal[c(18:45)] ## gridded covariates for model calibration from 20
 GRIDSv <- etm3_val[c(18:45)] ## same for 51 randomly selected validation Woredas
 
 # Random Forest models ----------------------------------------------------
-set.seed(1385321)
+# Start foreach to paralellize model fitting
+mc <- makeCluster(detectCores())
+registerDoParallel(mc)
 
-# Cross-validation setup
+# Control setup
+set.seed(1385321)
 tc <- trainControl(method = "oob")
 
 # V0 = ilr [P,K,S,Ca,Mg | Fv]
-V0.rf <- train(GRIDSc, V0, method = "rf", trControl = tc)
+V0.rf <- train(GRIDSc, V0, method = "rf", preProc = c("center", "scale"), trControl = tc)
 print(V0.rf)
 v0.imp <- varImp(V0.rf, useModel = FALSE)
 plot(v0.imp, top=28)
 
 # V3 = ilr [P,K | K,Ca,Mg]
-V3.rf <- train(GRIDSc, V3, method = "rf", trControl = tc)
+V3.rf <- train(GRIDSc, V3, method = "rf", preProc = c("center", "scale"), trControl = tc)
 print(V3.rf) 
 v3.imp <- varImp(V3.rf, useModel = FALSE)
 plot(v3.imp, top=28)
 
 # V4 = ilr [K | Ca,Mg]
-V4.rf <- train(GRIDSc, V4, method = "rf", trControl = tc)
+V4.rf <- train(GRIDSc, V4, method = "rf", preProc = c("center", "scale"), trControl = tc)
 print(V4.rf)
 v4.imp <- varImp(V4.rf, useModel = FALSE)
 plot(v4.imp, top=28)
 
 # V5 = ilr [P | S]
-V5.rf <- train(GRIDSc, V5, method = "rf", trControl = tc)
+V5.rf <- train(GRIDSc, V5, method = "rf", preProc = c("center", "scale"), trControl = tc)
 print(V5.rf)
 v5.imp <- varImp(V5.rf, useModel = FALSE)
 plot(v5.imp, top=28)
 
 # V6 = ilr [Ca | Mg]
-V6.rf <- train(GRIDSc, V6, method = "rf", trControl = tc)
+V6.rf <- train(GRIDSc, V6, method = "rf", preProc = c("center", "scale"), trControl = tc)
 print(V6.rf)
 v6.imp <- varImp(V6.rf, useModel = FALSE)
 plot(v6.imp, top=28)
+
+stopCluster(mc)
 
 # Test set predictions ----------------------------------------------------
 V0_rf <- predict(V0.rf, GRIDSv)
