4796161f7ac9911c5a53841738fd3db13d0d622c
mgwalsh <markusgwalsh@gmail.com>
2015-12-23 08:59:33 +0300

    Ensemble predictions

--- ETM3_ENS.R
+++ ETM3_ENS.R
@@ -70,8 +70,8 @@ V4_fit <- predict(V4.ens, etm3)
 V5_fit <- predict(V5.ens, etm3)
 V6_fit <- predict(V6.ens, etm3)
 fit <- cbind.data.frame(V0_fit,V3_fit,V4_fit,V5_fit,V6_fit)
-obs <- etm3[c("PID","V0","V3","V4","V5","V6")]
-ens_eval <- cbind(obs, fit)
+obs <- etm3[c("Woreda","PID","P","K","S","V0","V3","V4","V5","V6")]
+ens_fit <- cbind(obs, fit)
 
 # Gridded ensemble predictions --------------------------------------------
 V0_ens <- predict(grids, V0.ens)
